26110df5f83a5f2cf3704a438c678baaba6835f5
Luis Felipe <sv.luisfelipe@gmail.com>
2017-11-11 19:01:03 -0600

    Database scrip changes

--- data/database.sql
+++ data/database.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE IF NOT EXISTS motelSweet_db;
 
-
 CREATE TABLE motelSweet_db.Users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(30) NOT NULL,
@@ -15,23 +14,23 @@ CREATE TABLE motelSweet_db.Rooms (
 	id INT NOT NULL PRIMARY KEY,
 	type INT NOT NULL, -- 1, 2 or 3
 	price INT NOT NULL, -- linked with roomType
-    status INT NOT NULL, -- 1: available, 2: occupied, 3: inService, 4: inRepair
+    status INT NOT NULL -- 1: available, 2: occupied, 3: inService, 4: inRepair
 );
 
 CREATE TABLE motelSweet_db.BookingHistory (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	startDate TIMESTAMP NOT NULL,
 	endDate TIMESTAMP,
-	rooomId INT NOT NULL,
+	roomId INT NOT NULL,
     earning INT NOT NULL,
     extraEarning INT,
-    FOREIGN KEY (roomId) REFERENCES Rooms(id),
+    FOREIGN KEY (roomId) REFERENCES Rooms(id)
 );
 
 INSERT INTO Users(username, userPassword, email, firstName, lastName, admin)
 VALUES  ('admin', 'adminpassword', 'admin@mail.com', 'Nombre', 'apellido', 1);
 
-INSERT INTO Rooms(roomId, roomType, roomPrice, roomStatus)
+INSERT INTO Rooms(id, type, price, status)
 VALUES  (1, 1, 100, 1),
 		(2, 1, 100, 1),
 		(3, 1, 100, 1),
